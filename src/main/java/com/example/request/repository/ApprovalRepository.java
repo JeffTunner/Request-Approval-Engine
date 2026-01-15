60b5017f3cbc343bad05c66ee4224e96cac07d5d
JeffTunner <abhinavkaushik05@gmail.com>
2026-01-15 21:05:57 +0530

    Extra queries

--- src/main/java/com/example/request/repository/ApprovalRepository.java
+++ src/main/java/com/example/request/repository/ApprovalRepository.java
@@ -4,6 +4,10 @@ import com.example.request.entity.ApprovalStep;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ApprovalRepository extends JpaRepository<ApprovalStep, Long> {
+
+    List<ApprovalStep> findByRequestId(Long id);
 }

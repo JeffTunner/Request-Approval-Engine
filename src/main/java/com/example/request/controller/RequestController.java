71395559fd8f7f0914feec55cbc32fbf3e0c2a1a
JeffTunner <abhinavkaushik05@gmail.com>
2026-01-15 21:06:28 +0530

    Added remaining endpoints

--- src/main/java/com/example/request/controller/RequestController.java
+++ src/main/java/com/example/request/controller/RequestController.java
@@ -2,15 +2,22 @@ package com.example.request.controller;
 
 import com.example.request.dto.RequestDto;
 import com.example.request.dto.ResponseDto;
+import com.example.request.entity.AuditLog;
+import com.example.request.service.ApprovalService;
 import com.example.request.service.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class RequestController {
 
     @Autowired
     RequestService requestService;
+
+    @Autowired
+    ApprovalService approvalService;
 
     @PostMapping("/requests")
     public ResponseDto create(@RequestBody RequestDto requestDto) {
@@ -20,6 +27,21 @@ public class RequestController {
     @PostMapping("/requests/{id}/submit")
     public ResponseDto submit(@PathVariable Long id) {
         return requestService.submitRequest(id);
+    }
+
+    @PostMapping("/approvals/{stepId}/approve")
+    public String approve(@PathVariable Long stepId) {
+        return approvalService.approval(stepId);
+    }
+
+    @PostMapping("/approvals/{stepId}/reject")
+    public String reject(@PathVariable Long stepId) {
+        return approvalService.rejection(stepId);
+    }
+
+    @GetMapping("/requests/{id}/timeline")
+    public List<AuditLog> timeline(@PathVariable Long id) {
+        return requestService.timeline(id);
     }
 
 }
